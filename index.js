bdd42b3702a877a5fb47a39a1b8c9c61ed0b5002
Mariusz Nowak <medyk@medikoo.com>
2015-06-02 16:32:34 +0200

    Update up to changes in site-tree

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 'use strict';
 
-var forEach      = require('es5-ext/object/for-each')
-  , ensureObject = require('es5-ext/object/valid-object')
-  , getRouter    = require('controller-router')
-  , ensureView   = require('site-tree/ensure-view')
+var forEach        = require('es5-ext/object/for-each')
+  , ensureObject   = require('es5-ext/object/valid-object')
+  , getRouter      = require('controller-router')
+  , ensureSiteTree = require('site-tree/ensure')
 
   , create = Object.create, stringify = JSON.stringify;
 
 module.exports = function (viewMap, tree/*, options*/) {
 	var options = Object(arguments[2]), baseContext, notFoundView
-	  , routes = create(null), router;
+	  , routes = create(null), router, ensureView;
+	ensureObject(viewMap);
+	ensureSiteTree(tree);
+	ensureView = tree.constructor.ensureView.bind(tree.constructor);
 
 	if (options.context != null) baseContext = ensureObject(options.context);
-	if (options.notFound != null) notFoundView = ensureView(options.notFound, tree);
+	if (options.notFound != null) notFoundView = ensureView(options.notFound);
 	forEach(viewMap, function (viewConf, path) {
 		var view;
 		if (viewConf.match && viewConf.view) {
 			// Dynamic path
-			view = ensureView(viewConf.view, tree);
+			view = ensureView(viewConf.view);
 			routes[path] = {
 				match: viewConf.match,
 				controller: function () { tree.load(view, this); }
 			};
 		} else {
 			// Static path
-			ensureView(viewConf, tree);
+			ensureView(viewConf);
 			routes[path] = function () { tree.load(viewConf, this); };
 		}
 	});
