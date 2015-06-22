8e4301d8a4a748be0403c36588b29394203fd02e
Mariusz Nowak <medyk@medikoo.com>
2015-06-22 12:35:32 +0200

    Rename variables

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ var forEach        = require('es5-ext/object/for-each')
 
   , create = Object.create, stringify = JSON.stringify;
 
-module.exports = function (viewMap, tree/*, options*/) {
+module.exports = function (routesMap, siteTree/*, options*/) {
 	var options = Object(arguments[2]), baseContext, notFoundView
 	  , routes = create(null), router, ensureView;
-	ensureObject(viewMap);
-	ensureSiteTree(tree);
-	ensureView = tree.constructor.ensureView.bind(tree.constructor);
+	ensureObject(routesMap);
+	ensureSiteTree(siteTree);
+	ensureView = siteTree.constructor.ensureView.bind(siteTree.constructor);
 
 	if (options.context != null) baseContext = ensureObject(options.context);
 	if (options.notFound != null) notFoundView = ensureView(options.notFound);
-	forEach(viewMap, function (viewConf, path) {
+	forEach(routesMap, function (viewConf, path) {
 		var view;
 		if (viewConf.match && viewConf.view) {
 			// Dynamic path
 			view = ensureView(viewConf.view);
 			routes[path] = {
 				match: viewConf.match,
-				controller: function () { tree.load(view, this); }
+				controller: function () { siteTree.load(view, this); }
 			};
 		} else {
 			// Static path
 			ensureView(viewConf);
-			routes[path] = function () { tree.load(viewConf, this); };
+			routes[path] = function () { siteTree.load(viewConf, this); };
 		}
 	});
 	router = getRouter(routes);
@@ -37,7 +37,7 @@ module.exports = function (viewMap, tree/*, options*/) {
 		result = router.call(context, pathname);
 		if (result) return result;
 		if (!notFoundView) throw new Error(stringify(pathname) + ' route not found');
-		tree.load(notFoundView, context);
+		siteTree.load(notFoundView, context);
 		return result;
 	};
 };
