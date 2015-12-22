2f4d1cfef353d395a49ee66dec74e42bb42c3fbe
Mariusz Nowak <medyk@medikoo.com>
2015-12-22 16:57:00 +0100

    Update dependencies

--- test/__tad.js
+++ test/__tad.js
@@ -1,23 +1,10 @@
 'use strict';
 
-var ClassList = require('class-list')
-  , document;
+var document;
 
 try {
 	document = require('jsdom').jsdom();
 } catch (ignore) {}
-
-if (document) {
-	Object.defineProperty(document.defaultView.HTMLElement.prototype, 'classList', {
-		get: function () {
-			Object.defineProperty(this, 'classList',
-				{ value: null, writable: true, configurable: true });
-			Object.defineProperty(this, 'classList',
-				{ value: new ClassList(this), writable: true, configurable: true });
-			return this.classList;
-		}
-	});
-}
 
 exports.context = document ? {
 	document: document,
